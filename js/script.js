94a5f098936643a8d1fcea37dcb34338f2d8534c
Sardorbek Norqulov <152975289+bycooder@users.noreply.github.com>
2025-01-20 21:09:03 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,259 +1,33 @@
-let score = Number(0);
+let score = 0;
 const display = document.querySelector("#score");
-function numbers(input) {
-  input = Number(input);
 
-  if (input === 1) {
-    document.querySelector(".e").onclick = () => {
-      document.querySelector(".e").innerHTML = `1`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".p").onclick = () => {
-      document.querySelector(".p").innerHTML = `1`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".s").onclick = () => {
-      document.querySelector(".s").innerHTML = `1`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".e1").onclick = () => {
-      document.querySelector(".e1").innerHTML = `1`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".n1").onclick = () => {
-      document.querySelector(".n1").innerHTML = `1`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".f1").onclick = () => {
-      document.querySelector(".f1").innerHTML = `1`;
-      score += 10;
-      updateScore();
-    };
-  } else if (input === 2) {
-    document.querySelector(".c").onclick = () => {
-      document.querySelector(".c").innerHTML = `2`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".l").onclick = () => {
-      document.querySelector(".l").innerHTML = `2`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".q").onclick = () => {
-      document.querySelector(".q").innerHTML = `2`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".t").onclick = () => {
-      document.querySelector(".t").innerHTML = `2`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".k1").onclick = () => {
-      document.querySelector(".k1").innerHTML = `2`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".o1").onclick = () => {
-      document.querySelector(".o1").innerHTML = `2`;
-      score += 10;
-      updateScore();
-    };
-  } else if (input === 3) {
-    document.querySelector(".a").onclick = () => {
-      document.querySelector(".a").innerHTML = `3`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".k").onclick = () => {
-      document.querySelector(".k").innerHTML = `3`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".r").onclick = () => {
-      document.querySelector(".r").innerHTML = `3`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".d1").onclick = () => {
-      document.querySelector(".d1").innerHTML = `3`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".e1").onclick = () => {
-      document.querySelector(".e1").innerHTML = `3`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".s1").onclick = () => {
-      document.querySelector(".s1").innerHTML = `3`;
-      score += 10;
-      updateScore();
-    };
-  } else if (input === 4) {
-    document.querySelector(".d").onclick = () => {
-      document.querySelector(".d").innerHTML = `4`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".m").onclick = () => {
-      document.querySelector(".m").innerHTML = `4`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".x").onclick = () => {
-      document.querySelector(".x").innerHTML = `4`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".a1").onclick = () => {
-      document.querySelector(".a1").innerHTML = `4`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".m1").onclick = () => {
-      document.querySelector(".m1").innerHTML = `4`;
-      score += 10;
-      updateScore();
-    };
-  } else if (input === 5) {
-    document.querySelector(".f").onclick = () => {
-      document.querySelector(".f").innerHTML = `5`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".i").onclick = () => {
-      document.querySelector(".i").innerHTML = `5`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".y").onclick = () => {
-      document.querySelector(".y").innerHTML = `5`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".c1").onclick = () => {
-      document.querySelector(".c1").innerHTML = `5`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".i1").onclick = () => {
-      document.querySelector(".i1").innerHTML = `5`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".m1").onclick = () => {
-      document.querySelector(".m1").innerHTML = `5`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".j1").onclick = () => {
-      document.querySelector(".j1").innerHTML = `5`;
-      score += 10;
-      updateScore();
-    };
-  } else if (input === 6) {
-    document.querySelector(".j").onclick = () => {
-      document.querySelector(".j").innerHTML = `6`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".z").onclick = () => {
-      document.querySelector(".z").innerHTML = `6`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".g1").onclick = () => {
-      document.querySelector(".g1").innerHTML = `6`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".r1").onclick = () => {
-      document.querySelector(".r1").innerHTML = `6`;
-      score += 10;
-      updateScore();
-    };
-  } else if (input === 7) {
-    document.querySelector(".h").onclick = () => {
-      document.querySelector(".h").innerHTML = `7`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".o").onclick = () => {
-      document.querySelector(".o").innerHTML = `7`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".u").onclick = () => {
-      document.querySelector(".u").innerHTML = `7`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".q1").onclick = () => {
-      document.querySelector(".q1").innerHTML = `7`;
-      score += 10;
-      updateScore();
-    };
-  } else if (input === 8) {
-    document.querySelector(".b").onclick = () => {
-      document.querySelector(".b").innerHTML = `8`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".n").onclick = () => {
-      document.querySelector(".n").innerHTML = `8`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".g").onclick = () => {
-      document.querySelector(".g").innerHTML = `8`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".v").onclick = () => {
-      document.querySelector(".v").innerHTML = `8`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".h1").onclick = () => {
-      document.querySelector(".h1").innerHTML = `8`;
-      score += 10;
-      updateScore();
-    };
-  } else if (input === 9) {
-    document.querySelector(".u1").onclick = () => {
-      document.querySelector(".u1").innerHTML = `9`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".t1").onclick = () => {
-      document.querySelector(".t1").innerHTML = `9`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".b1").onclick = () => {
-      document.querySelector(".b1").innerHTML = `9`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".l1").onclick = () => {
-      document.querySelector(".l1").innerHTML = `9`;
-      score += 10;
-      updateScore();
-    };
-    document.querySelector(".p1").onclick = () => {
-      document.querySelector(".p1").innerHTML = `9`;
-      score += 10;
-      updateScore();
-    };
-  }
+const elementsMap = {
+    1: [".e", ".p", ".s", ".e1", ".n1", ".f1"],
+    2: [".c", ".l", ".q", ".t", ".k1", ".o1"],
+    3: [".a", ".k", ".r", ".d1", ".e1", ".s1"],
+    4: [".d", ".m", ".x", ".a1", ".m1"],
+    5: [".f", ".i", ".y", ".c1", ".i1", ".m1", ".j1"],
+    6: [".j", ".z", ".g1", ".r1"],
+    7: [".h", ".o", ".u", ".q1"],
+    8: [".b", ".n", ".g", ".v", ".h1"],
+    9: [".u1", ".t1", ".b1", ".l1", ".p1"],
+};
+
+function numbers(input) {
+    input = Number(input);
+
+    if (elementsMap[input]) {
+        elementsMap[input].forEach((selector) => {
+            const element = document.querySelector(selector);
+            element.onclick = () => {
+                element.innerHTML = `${input}`;
+                score += 10;
+                updateScore();
+            };
+        });
+    }
 }
+
 function updateScore() {
-  display.innerHTML = `point:${score}`;
+    display.innerHTML = `point: ${score}`;
 }
